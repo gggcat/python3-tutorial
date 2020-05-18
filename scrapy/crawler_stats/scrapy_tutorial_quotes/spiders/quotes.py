37631a1d8a8a8e5a5703ee58ddf2a81c2bf3f8fd
gggcat <61681628+gggcat@users.noreply.github.com>
2020-05-18 13:35:11 +0900

    scrapyの統計情報操作のチュートリアル

---
+++ scrapy/crawler_stats/scrapy_tutorial_quotes/spiders/quotes.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        #print(self.crawler.stats.get_stats())
+        self.crawler.stats.inc_value('crawled_pages')
+        for quote in response.css('div.quote'):
+            self.crawler.stats.inc_value('crawled_items')
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
