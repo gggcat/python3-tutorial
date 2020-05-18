c9a2abb4b4bf5d5e70f8ccc85e4231d67ca99605
gggcat <61681628+gggcat@users.noreply.github.com>
2020-05-18 16:40:48 +0900

    Scrapyのシングナル処理のチュートリアル

---
+++ scrapy/spider_signals/scrapy_tutorial_quotes/spiders/quotes.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy import signals
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(QuotesSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        return spider
+
+    def spider_closed(self, spider):
+        spider.logger.info('Spider closed!!!!!!!!!: %s', spider.name)
+        spider.logger.info(spider.crawler.stats.get_stats())
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
